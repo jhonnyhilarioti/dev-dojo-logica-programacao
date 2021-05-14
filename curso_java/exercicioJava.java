d7614877703ddc3a2e03af4f24d1c0244395910f
Jhonny Hilario Goulart <jhonnyhilarioti@gmail.com>
2021-05-14 15:24:48 -0300

    exercício de Java

---
+++ curso_java/exercicioJava.java
@@ -0,0 +1,15 @@
+/*
+Crie uma classe chamada CalculadorDiferencaIdade
+Crie uma variável para gurdar a primeira idade
+Crie uma variável para guardara segunda idade
+crie uma variável para guardar a diferença entre a idade 1 e a idade 2
+Imprima a diferença no console
+*/
+public class CalculadorDiferencaIdade{
+    public static void main(String[] arg){
+        int primeiraIdade = 30;
+        int segundaIdade = 15;
+        int diferencaIdades = primeiraIdade - segundaIdade;
+        System.out.println("A diferença entre as idades eh: "+ diferencaIdades);
+    }
+}
