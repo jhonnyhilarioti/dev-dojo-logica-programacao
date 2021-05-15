bea327a3f0e140f45936a98ac5a8416890942b4d
Jhonny Hilario Goulart <jhonnyhilarioti@gmail.com>
2021-05-15 15:34:39 -0300

    switchCase para os dias da semana

---
+++ curso_java/SwitchCase.java
@@ -0,0 +1,29 @@
+public class SwitchCase {
+    public static void main(String[] args) {
+        int dia = 3;
+        switch (dia){
+            case 1:
+                System.out.println("Segunda");
+                break;
+            case 2:
+                System.out.println("Terça");
+                break;
+            case 3:
+                System.out.println("Quarta");
+                break;
+            case 4:
+                System.out.println("Quinta");
+                break;
+            case 5:
+                System.out.println("Sexta");
+                break;
+            case 6:
+                System.out.println("Sabado");
+                break;
+            case 7:
+                System.out.println("Domingo");
+                break;
+        }
+        System.out.println("Fim do Programa!");
+    }
+}
