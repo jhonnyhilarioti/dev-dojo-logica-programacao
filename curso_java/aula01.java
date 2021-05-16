d4898108d784138a9bf62a0d2d4358b4e961935a
Jhonny Hilario Goulart <jhonnyhilarioti@gmail.com>
2021-05-16 13:32:10 -0300

    corrigindo erro de caracteres especiais

---
+++ curso_java/aula01.java
@@ -0,0 +1,11 @@
+package LacosDeRepeticao;
+
+public class aula01 {
+    public static void main(String[] args) {
+        int contador = 0;
+        while(contador <= 10){
+            System.out.println("Contador: "+ contador);
+            contador = contador + 1;
+        }
+    }
+}
