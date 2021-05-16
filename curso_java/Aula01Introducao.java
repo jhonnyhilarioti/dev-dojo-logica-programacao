130d2d77a9f0d4d82ab6a87e1af600d7a7c908d6
Jhonny Hilario Goulart <jhonnyhilarioti@gmail.com>
2021-05-16 13:50:00 -0300

    Escrevendo de 0 a 1 com FOR

---
+++ curso_java/Aula01Introducao.java
@@ -0,0 +1,9 @@
+package LacosDeRepeticao.fors;
+
+public class Aula01Introducao {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 10; i++ ){
+            System.out.println(i);
+        }
+    }
+}
