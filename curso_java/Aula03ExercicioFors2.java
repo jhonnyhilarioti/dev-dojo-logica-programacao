9c49ebf83a172b58ef4095dcc7c69dac3c73a52e
Jhonny Hilario Goulart <jhonnyhilarioti@gmail.com>
2021-05-16 14:20:14 -0300

    Tabuada de 1 a 10 com FOR

---
+++ curso_java/Aula03ExercicioFors2.java
@@ -0,0 +1,12 @@
+package LacosDeRepeticao.fors;
+
+public class Aula03ExercicioFors2 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10 ; i++) {
+            System.out.println("Fazendo a tabuada do numero: "+ i);
+            for (int j = 1; j <= 10 ; j++) {
+                System.out.println(i + " x " + j + " = " + (i * j));
+            }
+        }
+    }
+}
