c8189d623eef32d25ae5f419c2afb3580d996833
Jhonny Hilario Goulart <jhonnyhilarioti@gmail.com>
2021-05-16 14:04:41 -0300

    Tabuada com FOR

---
+++ curso_java/Aula02ExercicioFors.java
@@ -0,0 +1,9 @@
+package LacosDeRepeticao.fors;
+
+public class Aula02ExercicioFors {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10 ; i++) {
+            System.out.println(5 * i);
+        }
+    }
+}
